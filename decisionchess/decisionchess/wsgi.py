58bb689768f215f7717b429f49ddf528a702b692
kevinjorge <kevinjorge10@hotmail.com>
2023-10-26 20:27:43 -0400

    remove vercel deps

--- decisionchess/decisionchess/wsgi.py
+++ decisionchess/decisionchess/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decisionchess.settings')
 
 application = get_wsgi_application()
-
-app = application # Vercel needs this to recognize application
