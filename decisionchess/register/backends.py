98bbe75ce856acf966982bf52a5008becc566f24
kevinjorge <kevinjorge10@hotmail.com>
2024-01-23 15:20:41 -0500

    email login, inactive purge command

--- decisionchess/register/backends.py
+++ decisionchess/register/backends.py
@@ -6,10 +6,23 @@ User = get_user_model()
 
 class CustomAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        user = super().authenticate(request, username, password, **kwargs)
+
+        UserModel = get_user_model()
+
+        # Check if the input is an email address
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+
+        try:
+            user = UserModel.objects.get(**kwargs)
+        except UserModel.DoesNotExist:
+            return None
+
+        if user.check_password(password) and not user.bot_account:
+            return user
 
         if user and user.bot_account:
             # Prevent login for bot users
             raise PermissionDenied("Unauthorized")
-
-        return user
