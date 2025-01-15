565a943fec4785ca8608df7b9d96245b78349944
kevinjorge <kevinjorge10@hotmail.com>
2025-01-14 21:54:01 -0500

    Update views.py

--- decisionchess/main/views.py
+++ decisionchess/main/views.py
@@ -40,7 +40,7 @@ def get_static_file_path(file_name):
     if settings.DEBUG:
         return f'.\\static\\images\\{file_name}'
     else:
-        print(os.listdir('static_collected_files\\images'))
+        print(os.listdir(os.path.join('static_collected_files', 'images')))
         return f'.\\static_collected_files\\images\\{file_name}'
 
 def index(request):
