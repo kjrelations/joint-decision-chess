558f98d99268a0665ad380d8a12c5720e5dc365f
kevinjorge <kevinjorge10@hotmail.com>
2025-01-15 20:44:26 -0500

    Update settings.py

--- decisionchess/decisionchess/settings.py
+++ decisionchess/decisionchess/settings.py
@@ -34,8 +34,11 @@ if DEBUG:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR.parent.parent / 'debug_images'
 else:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+        },
+    }
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
